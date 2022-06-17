87a38f056393bc825abef063afe86e8c6d24b14b
fjf <840420093@qq.com>
2022-06-17 19:58:46 +0800

    extract github information

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 ## 学习【社区】项目
 
 ## 资料
-https://spring.io/guides
-https://spring.io/guides/gs/serving-web-content/
-https://elasticsearch.cn/explore
-https://www.bilibili.com/video/BV1r4411r7au?p=4&spm_id_from=pageDriver&vd_source=3834b5a51ba750b023793a2b7a0fb73e
-https://github.com/codedrinker/community
+[Spring](https://spring.io/guides)
+[Spring web](https://spring.io/guies/gs/serving-web-content/)
+[ES社区](https//elasticsearch.cn/explore)
+[B站视频](https://www.bilibili.com/video/BV1r4411r7au?p=4&spm_id_from=pageDriver&vd_source=3834ba51ba750b023793a2b7a0fb73e)
+[Github源码地址](https://githu.com/codedrinker/community)
 
-# 授权github的登录
-https://docs.github.com/en/developers/apps/building-oauth-apps/creating-an-oauth-app
+## 工具
+[授权github的登录](https://docs.github.com/en/developers/apps/building-oauth-apps/creating-an-oauth-app)
+
+
+## 记录快捷键
+- ctrl+shift+f12：代码全屏
+- 按住alt然后鼠标下拉，对这一列进行相同操作
+- Fn+end跳转到最后一列
---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,41 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect_url}")
+    private String redirectUrl;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_url(redirectUrl);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        //github未设置名字的时候也是null
+        System.out.println(user.getName());
+        return "index";
+    }
+}
